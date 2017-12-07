db251d19be0b037099008c8af2515f810555ab67
Conor Riches <conor.riches@bbc.co.uk>
2017-12-07 01:54:11 +0000

    create-react-app, Screen container, timings

---
+++ src/components/Screen.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "../App.css";
+import Transition from "react-transition-group/Transition";
+
+class Screen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
+
+  componentDidUpdate() {
+    const active = this.props.active;
+    const elemName = this.props.children.type.name;
+    console.log(elemName);
+
+    if (
+      (active == elemName && !this.state.show) ||
+      (active != elemName && this.state.show)
+    )
+      this.setState({ show: active == elemName });
+    //console.log(active == elemName);
+  }
+
+  render() {
+    return (
+      <Transition timeout="1000">
+        {status => {
+          return this.state.show ? (
+            <div className={`screen fade fade-${status} ${this.props.children.type.name}`}>{this.props.children}</div>
+          ) : (
+            <div className={`screen fade`}/>
+          );
+        }}
+      </Transition>
+    );
+  }
+}
+
+export default Screen;
