6f5dddf975bbbe42f32522d9a205edfea185bb22
Conor Riches <conor.riches@bbc.co.uk>
2017-12-27 21:02:05 +0000

    Update time and add to footer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+describe("startup", () => {
+  it("should initiate an empty state set", () => {
+    expect (App.state).toBe({
+      index: 0,
+      slide: "",
+      hide: 0,
+      doorOpen: false,
+      lastEntered: [],
+      notification: null,
+      doorbell: 0,
+      emergency: null,
+      audio: {
+        doorbell: 0,
+        entry: 0,
+        emergency: 0
+      },
+      metrolink: {}
+    });
+  });
 });
--- src/screens/Time.jsx
+++ src/screens/Time.jsx
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
+import TimeComponent from '../components/Time';
 
 class Time extends Component {
-  getTime() {
-    const now = new Date();
-    let hours = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
-    let mins = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
-    return `${hours}:${mins}`
-  }
-
   render() {
     return (
       <div>
         <h2>
           It is currently
-          <div className="Time__time">{this.getTime()}</div>
+          <div className="Time__time"><TimeComponent/></div>
           on
           <div className="Time__date">{new Date().toDateString()}</div>
         </h2>
