e59c7f737ebaddbb8eb4422fb67144eb703cfde6
Conor Riches <conor.riches@bbc.co.uk>
2017-12-27 00:30:21 +0000

    finishing touches

--- src/screens/Metrolink.jsx
+++ src/screens/Metrolink.jsx
@@ -9,8 +9,7 @@ class Metrolink extends Component {
   }
 
   componentDidUpdate(p) {
-
-    console.log(p)
+    console.log(p);
     if (this.props.parentState.metrolink.platforms && this.state.gotData === 1)
       return;
     this.props.parentState.metrolink.platforms
@@ -28,29 +27,46 @@ class Metrolink extends Component {
           <div>
             <div className="Metrolink__platforms">
               {this.props.parentState.metrolink.platforms.map(platform => {
-                console.log("Platform", platform);
-
                 return (
                   <div className="Metrolink__platform">
-                    <div className="Metrolink__direction">{platform.direction}</div>
-                    <ul>
-                      {platform.trams.map(tram => {
-                        return (
+                    
+                    <div className="Metrolink__direction">
+                      {platform.direction}
+                    </div>
+
+                    <ul className="Metrolink__trams">
+                      {platform.trams.map((tram, i) => {
+                        return tram.destination ? (
                           <li className="Metrolink__tram">
                             <b>
                               {tram.wait == 0 ? `arrv` : `${tram.wait} min`}
                             </b>{" "}
                             - {tram.destination}
                           </li>
+                        ) : (
+                          <li className="Metrolink__tram">
+                            <b>
+                              {i == 0
+                                ? "No trams scheuled to depart within the next half an hour."
+                                : ""}
+                            </b>
+                          </li>
                         );
                       })}
                     </ul>
+
+                    <div className="Metrolink__messageBoard">
+                      {platform.messageBoard}
+                    </div>
+                    
                   </div>
                 );
               })}
             </div>
 
-            <div className="Metrolink__lastUpdated">Updated @ {this.props.parentState.metrolink.lastUpdated}</div>
+            <div className="Metrolink__lastUpdated">
+              Updated @ {this.props.parentState.metrolink.lastUpdated}
+            </div>
           </div>
         ) : (
           "No data"
--- src/screens/Time.jsx
+++ src/screens/Time.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 
 class Time extends Component {
-  render() {
+  getTime() {
     const now = new Date();
+    let hours = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
+    let mins = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+    return `${hours}:${mins}`
+  }
+
+  render() {
     return (
       <div>
         <h2>
           It is currently
-          <div className="Time__time">{`${now.getHours()}:${now.getMinutes()}`}</div>
+          <div className="Time__time">{this.getTime()}</div>
           on
-          <div className="Time__date">{now.toDateString()}</div>
+          <div className="Time__date">{new Date().toDateString()}</div>
         </h2>
       </div>
     );
