e9552190ef8db926065fcc7ebe74eaa22d861a9e
Conor Riches <conor.riches@bbc.co.uk>
2018-01-01 17:57:01 +0000

    Make information on Met + Wifi clearer

--- src/screens/WiFi.jsx
+++ src/screens/WiFi.jsx
@@ -4,9 +4,10 @@ class WiFi extends Component {
   render() {
     return (
       <div>
-        <h1>WiFi</h1>
-        <h3>SSID: Hackspace</h3>
-        <h3>Password: T3h4x0rZ</h3>
+        <h1>WiFi Details</h1>
+        <h4>Network and Password:</h4>
+        <h3>Hackspace</h3>
+        <h3>T3h4x0rZ</h3>
 
         <img src="/assets/angelparrot.gif" alt=""/>
       </div>
